c3ee6f05e6d9858951dcc8fdad6ca228f4ce4894
artstar10 <arthurnevesx@hotmail.com>
2021-04-17 19:15:41 -0300

    chore: tipos booleanos

--- typescript/declarar-tipos-de-variaveis-em-typeScript/tipos.ts
+++ typescript/declarar-tipos-de-variaveis-em-typeScript/tipos.ts
@@ -2,3 +2,9 @@ let firstName: string = "Arthur";
 let sentence: string = `My name is ${firstName}.
     I am new to TypeScript.`;
 console.log(sentence);
+
+// Tipos booleanos
+// O tipo de dados mais básico é o valor true ou false, conhecido como booliano.
+let flag: boolean;
+let yes = true;
+let no = false;
